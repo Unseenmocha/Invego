f1a6608bdc04dd92603672dd6bc8665a714ec637
Andrew Lin <unseenmocha@gmail.com>
2023-04-07 23:49:43 -0400

    styling

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import { getBittels } from './getInfo.js';
 function header() {
     const headerID = document.getElementById("header");
     const headerLeftContent = '<div class="header-left"><a href="#">Invego</a></div>';
-    const headerBittels = `<div class="header-right"><span class="bittels-text">Bittels:</span> <span class="bittels-text">${getBittels()}</span></div>`;
+    const headerBittels = `<div class="header-right"></img><span class="bittels-text">Bittels: </span><span class="bittels-text">${getBittels()}</span></div>`;
     const headerContent = `<div class="App-header">${headerLeftContent}${headerBittels}</div>`;
     headerID.innerHTML = headerContent;
 }
