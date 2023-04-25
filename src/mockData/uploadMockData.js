1328406f90e205cc03b87f06a67d46d6a9621fe4
Andrew Lin <unseenmocha@gmail.com>
2023-04-25 17:32:12 -0400

    bulk upload portfolio mock data and user mock data via single function

--- src/mockData/uploadMockData.js
+++ src/mockData/uploadMockData.js
@@ -1,21 +1,20 @@
-let db1Old = new PouchDB("users");
-let db2Old = new PouchDB("portfolios");
+const uploadFileToDB = function(path, dbString) {
+    const xhrUser = new XMLHttpRequest();
+    xhrUser.open('GET', path);
+    xhrUser.responseType = 'json';
+    xhrUser.onload = async () => {
+        const data = xhrUser.response;
+        let dbOld = new PouchDB(dbString);
+        await dbOld.destroy();
+        let db = new PouchDB(dbString);
+        await db.bulkDocs(data).then(result => {
+            console.log('Bulk upload successful:', result);
+        }).catch(error => {
+            console.error('Bulk upload error:', error);
+        });
+    };
+    xhrUser.send();
+}
 
-
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '../../mockData/USER_MOCK_DATA.json');
-xhr.responseType = 'json';
-xhr.onload = async () => {
-    const data = xhr.response;
-    await db1Old.destroy();
-    //await db2Old.destroy();
-    let db1 = new PouchDB("users");
-    //let db2 = new PouchDB("portfolios");
-    await db1.bulkDocs(data).then(result => {
-        console.log('Bulk upload successful:', result);
-    }).catch(error => {
-        console.error('Bulk upload error:', error);
-    });
-};
-xhr.send();
-
+uploadFileToDB('../../mockData/USER_MOCK_DATA.json', 'users');
+uploadFileToDB('../../mockData/PORTFOLIO_MOCK_DATA.json', 'portfolios');
