c4017666e2f7bf649d8592891fd2cc3eca3a0783
Andrew Lin <unseenmocha@gmail.com>
2023-04-25 20:29:07 -0400

    starting to get old styling work to work with newest project version

--- src/client/Discovery/discovery.js
+++ src/client/Discovery/discovery.js
@@ -14,13 +14,13 @@ const populatePopularProfiles = async () => {
         table.innerHTML += 
         `<tr class="main-row">
             <td>
-                <img id="profile-pic" src="../../../assets/istockphoto-1130884625-612x612.jpeg" />
+                <img class="profile-pic" src="../../../assets/istockphoto-1130884625-612x612.jpeg" />
             </td>
             <td>
-                <text class="text-1">${lst.rows[i].doc.username}</text>
+                <p class="text-normal">${lst.rows[i].doc.username}</p>
             </td>
             <td>
-                <text class="text-1">${lst.rows[i].doc.bittels}</text>
+                <p class="text-normal">${lst.rows[i].doc.bittels}</p>
             </td>
         </tr>`
     }
