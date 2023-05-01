8ef44ea4d2780a134bdd19ddd84cda6deefa50b9
Brandon  Saulnier <124940268+b-saulnier@users.noreply.github.com>
2023-05-01 16:15:31 -0400

    added authentication to portfolio crud operations

--- src/server/routes/portfolios.js
+++ src/server/routes/portfolios.js
@@ -1,14 +1,17 @@
 import express from 'express'
 
 import { createPortfolio, readPortfolio, updatePortfolio, buy, sell, deletePortfolio } from '../controllers/users';
+import { auth }  from '../middleware/auth.js';
+
+
 export const router = express.Router();
 
 
 // routing for crud operations for within the portfolio
-router.post('/', createPortfolio);
-router.get('/:id', readPortfolio);
-router.put('/:id', updatePortfolio);
-router.put('/buy/:id', buy);
-router.put('/sell/:id', sell);
-router.delete('/:id', deletePortfolio);
+router.post('/', auth, createPortfolio);
+router.get('/:id', auth, readPortfolio);
+router.put('/:id', auth, updatePortfolio);
+router.put('/buy/:id', auth, buy);
+router.put('/sell/:id', auth, sell);
+router.delete('/:id', auth, deletePortfolio);
 
