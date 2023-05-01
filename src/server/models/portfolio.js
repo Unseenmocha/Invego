807b347a8772910ef918b5f793517b677bac4a19
Brandon  Saulnier <124940268+b-saulnier@users.noreply.github.com>
2023-05-01 14:45:30 -0400

    defined user and portfolio schemas

---
+++ src/server/models/portfolio.js
@@ -0,0 +1,15 @@
+// the models folder holds the course schema (structure of data stored in database) defined for each data type
+
+import mongoose from 'mongoose';
+
+
+export const portfolioSchema = mongoose.Schema({
+    _id: Number,
+    market_value: Number,
+    total_shares: Number,
+    percent_growth: String,
+});
+
+
+export const Portfolio = mongoose.model('Portfolio', portfolioSchema)
+
