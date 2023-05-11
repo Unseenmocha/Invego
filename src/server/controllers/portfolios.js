5aa361c200724e9a3f65f2ab1a76b9e0c2e51093
Quang Dang <qdang@umass.edu>
2023-05-11 19:31:59 -0400

    Wrote some CRUD for controller

--- src/server/controllers/portfolios.js
+++ src/server/controllers/portfolios.js
@@ -8,3 +8,35 @@ export const getPortfolio = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 };
+
+export const createPortfolio = async (req, res) => {
+    const portfolio = req.body;
+    const newPortfolio = new Portfolio(portfolio);
+    try {
+        await newPortfolio.save();
+        res.status(201).json(newPortfolio);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const deletePortfolio = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Portfolio.findByIdAndRemove(id).exec();
+        res.send('Successfully deleted!');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePortfolio = async (req, res) => {
+    const id = req.params.id;
+    const updates = req.body;
+    try {
+        const updatePortfolio = await Portfolio.findByIdAndUpdate(id, updates);
+        res.send(updatePortfolio);
+    }  catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
