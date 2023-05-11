31c63f3cfe582f0ae663a06eb18816d0351bdbc0
Andrew Lin <unseenmocha@gmail.com>
2023-05-11 19:11:33 -0400

    initial database connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,47 @@
 import express from 'express';
-import mongoose from'mongoose'; // are we using mongoose?
+import dotenv from 'dotenv';
+dotenv.config({path: '../../process.env'});
+import mongoose from'mongoose'; 
 // I don't know if we are allowed to use any more defined libraries
 
 import { userRoutes } from './routes/users.js';
 import { portfolioRoutes } from './routes/portfolios.js';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 
 /*
-   Here we should add middleware e.g app.use(middleware)
+   Here we add middleware e.g app.use(middleware)
 */
 
+app.use('/', userRoutes);
+app.use('/', portfolioRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
 
-app.use('/user', userRoutes);
-app.use('/portfolio', portfolioRoutes);
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   next();
+ });
 
 /*
-   Here we should connect to the database 
+   Here we connect to the database 
 */
+
+mongoose
+  .connect(process.env.DB.toString(), { useNewUrlParser: true })
+  .then(() => {
+    console.log("Database successfully connected!");
+    //start server
+    app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
+mongoose.Promise = global.Promise;
+
 
