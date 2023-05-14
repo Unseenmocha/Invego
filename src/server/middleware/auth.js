7573017b03e121f06731c875f84fdb7be12e0a19
Quang Dang <qdang@umass.edu>
2023-05-13 20:15:36 -0400

    Finished authentication.js

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -8,8 +8,29 @@ const { Strategy } = passportLocal;
 
 const users = await User.find();
 
-const strategy = new Strategy(async (username,password,done) => {
-//
+function findUser(username) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === username) {
+      return {found: true, user: users[i]};
+    }
+  }
+  return {found: false, user: null};
+}
+
+const strategy = new Strategy(async (username,password,done) => { 
+  let {found, user} = findUser(username);
+  if (found) {
+    if (user.password === password) {
+      console.log("passwords match");
+      return done(null, username);
+    } else {
+      console.log("passwords do not match");
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+  } else {
+    console.log("user not found");
+    return done(null, false, { message: 'Incorrect username.' });
+  }
 });
 
 
@@ -27,5 +48,8 @@ export default {
   configure: (app) => {
     app.use(passport.initialize());
     app.use(passport.session());
-  }
+  },
+  authenticate: (domain, where) => {
+    return passport.authenticate(domain, where);
+  },
 }
