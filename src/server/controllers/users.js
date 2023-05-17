69d5c6e0156e4ef9a4872a7c389f5c4e87e03265
Quang Dang <qdang@umass.edu>
2023-05-17 01:01:04 -0400

    Successfully implemented hashing for authentication

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -1,9 +1,15 @@
 import { User } from '../models/users.js';
 import { createPortfolioByUsername } from './portfolios.js';
 import { Portfolio } from '../models/portfolios.js';
+import crypto from 'crypto';
 
 // this file sends the json files to the frontend for each of the crud operations
 
+
+function validPassword(password, stored_pass, salt) {
+    let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
+    return stored_pass === hash;
+};
 
 export const getUsers = async (req, res) => {
     try {
@@ -44,7 +50,8 @@ export const createUser = async (req, res) => {
         firstName: "",
         lastName: "",
         username: req.body.username,
-        password: req.body.password,
+        password: "",
+        salt:"",
         bittels: 500,
         market_value: 50,
         total_shares: 100,
@@ -52,6 +59,7 @@ export const createUser = async (req, res) => {
     };
 
     const newUser = new User(user);
+    newUser.setPassword(req.body.password);
     console.log("newUser");
     try {
         await newUser.save();       
@@ -106,8 +114,7 @@ export const login = async (req, res) => {
 
     try {
         const foundUser = await User.findOne({ username: username }).lean();
-        console.log("found user", foundUser);
-        if (foundUser.password == password) {
+        if (validPassword(password, foundUser.password, foundUser.salt)) {
             res.status(200).json( foundUser );
         } else {
             console.log("incorrect password")
