d67e41590479aa75568cb6490d4109b3185b2bc4
Quang Dang <qdang@umass.edu>
2023-05-17 16:11:39 -0400

    Fixed salt

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -59,7 +59,7 @@ export const createUser = async (req, res) => {
     };
 
     const newUser = new User(user);
-    newUser.salt = crypto.randomBytes(16).toString('hex');
+    newUser.setPassword(user.password);
     try {
         await newUser.save();       
         console.log("newUser after save", newUser);
